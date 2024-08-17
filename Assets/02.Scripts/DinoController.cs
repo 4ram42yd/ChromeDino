5321feb975d868669e77a569f537325be70bc04a
4ram42yd <goydgo@gmail.com>
2024-08-18 02:44:02 +0900

    8_18

--- Assets/02.Scripts/DinoController.cs
+++ Assets/02.Scripts/DinoController.cs
@@ -8,24 +8,28 @@ public class DinoController : MonoBehaviour
     public bool isGrounded;
     public bool isDown;
 
+    public Transform groundCheckPoint;  // 빨간 점의 위치
+    public LayerMask whatIsGround;       // Ground인지 비교할 LayerMask
+
     private Animator anim;
+    private Rigidbody2D rb;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         anim.SetBool("isGround", true); // 처음에 Run 애니메이션 세팅
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, 0.2f, whatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded.Equals(true))
         {
-            anim.SetBool("isGround", false); // Space바 키 누를 때 Jump 애니메이션 세팅
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Dino의 가속도를 y방향으로 jumpForce만큼 준다.
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            anim.SetBool("isGround", true); // Space바 키 뗄 때 Run 애니메이션 세팅
-        }
+        anim.SetBool("isGround", isGrounded); // isGrounded의 값에 따라 자동으로 애니메이션 실행
     }
 }
