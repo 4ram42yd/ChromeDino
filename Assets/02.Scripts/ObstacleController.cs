6223da93972608aff843b2d7a8bb10ff53608665
4ram42yd <goydgo@gmail.com>
2024-08-20 21:54:36 +0900

    8_20

---
+++ Assets/02.Scripts/ObstacleController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public float moveSpeedX;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeedX, 0f);  // 리지드바디의 선형 속도(초당 단위)를 나타냅니다.
+    }
+}
+
